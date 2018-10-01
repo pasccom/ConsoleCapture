8a09eb6ccd1924e86dbbe2b9b49e1afae24368fb
Pascal COMBES <pascom@orange.fr>
2018-10-01 16:57:31 +0200

    Added clean function for arguments
    Also renamed function into caller and level into callee.

--- capture_console.js
+++ capture_console.js
@@ -7,7 +7,41 @@
  * The original functions are still available using `.original`
  * \param objName The name of the object to capture.
  */
-function capture(objName) {
+function capture(objName)
+{
+    function isA(value, typeName)
+    {
+        return Object.prototype.toString.call(value) == '[object ' + typeName + ']'
+    }
+
+    function clean(value)
+    {
+        if (isA(value, 'Arguments')) {
+            return Array.prototype.map.call(value, clean);
+        } else if (isA(value, 'Array')) {
+            return value.map(value.clean);
+        } else if (isA(value, 'Boolean') || isA(value, 'Number') || isA(value, 'String')) {
+            return value;
+        } else if (isA(value, 'Function')) {
+            return 'function()';
+        } else if (isA(value, 'Object')) {
+            var cleanValue = {};
+            Object.keys(value).forEach((key) => {
+                cleanValue[key] = clean(value[key]);
+            });
+            return cleanValue;
+        } else if (isA(value, 'Error')) {
+            return {
+                type: 'Error',
+                lineNumber: value.lineNumber,
+                columnNumber: value.columnNumber,
+                fileName: value.fileName,
+            };
+        } else {
+            return Object.prototype.toString.call(value);
+        }
+    }
+
     var captured = [];
 
     var obj = window[objName];
@@ -28,16 +62,16 @@ function capture(objName) {
             obj[key](... arguments);
 
             var cap = {
-                level: key,
+                callee: key,
                 time: Date.now(),
-                arguments: Array.prototype.slice.call(arguments),
+                arguments: clean(arguments),
             };
 
             var stack = (new Error()).stack.split('\n');
             var stackLineFields = (/^([^@]*)@(.*):(\d+):(\d+)$/).exec(stack[1]);
 
             if (stackLineFields !== null) {
-                cap.function = stackLineFields[1];
+                cap.caller = stackLineFields[1];
                 cap.fileName = stackLineFields[2];
                 cap.lineNumber = stackLineFields[3];
                 cap.columnNumber = stackLineFields[4];
